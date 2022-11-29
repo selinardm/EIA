4aa338df4c3c122c0295e9119df435c496190fc8
selinardm <selina.rademann@hs-furtwangen.de>
2022-11-29 19:52:17 +0100

    aufgabe7

--- Aufgabe 7/aufgabe7.js
+++ Aufgabe 7/aufgabe7.js
@@ -33,73 +33,29 @@ let WachstumgES = (populationES22 - populationES20);
 let ProzentEU = (1 - populationEU20 / populationEU22) * 100;
 let WachstumgEU = (populationEU22 - populationEU20);
 document.querySelector("#de").addEventListener("click", function () {
-    de22();
+    changeTo(populationDE22, populationDE20, ProzentDE, WachstumgDE, "stylede");
 });
-function de22() {
-    gesamt.innerHTML = populationDE22.toString();
-    gesamttext.innerHTML = "Gesamtzahl EinwohnerInnen in Deutschland in 2022 in Mio";
-    relativ.innerHTML = populationDE20.toString();
-    relativtext.innerHTML = "Gesamtzahl EinwohnerInnen in Deutschland im Jahr 2020 in Mio";
-    wachstum.innerHTML = ProzentDE.toFixed(2).toString();
-    wachstumtext.innerHTML = "Wachstumsrate seit 2022 in Prozent";
-    wachstumgesamt.innerHTML = WachstumgDE.toFixed(2).toString();
-    wachstumgesamttext.innerHTML = "Wachstumsrate gesamt zwischen 2020 und 2022 in Mio";
-    document.getElementById("chart").className = "stylede";
-}
 document.querySelector("#fr").addEventListener("click", function () {
-    fr22();
+    changeTo(populationFR22, populationFR20, ProzentFR, WachstumgFR, "stylefr");
 });
-function fr22() {
-    gesamt.innerHTML = populationFR22.toString();
-    gesamttext.innerHTML = "Gesamtzahl EinwohnerInnen in Frankreich in 2022 in Mio";
-    relativ.innerHTML = populationFR20.toString();
-    relativtext.innerHTML = "Gesamtzahl EinwohnerInnen in Frankreich im Jahr 2020 in Mio";
-    wachstum.innerHTML = ProzentFR.toFixed(2).toString();
-    wachstumtext.innerHTML = "Wachstumsrate seit 2022 in Prozent";
-    wachstumgesamt.innerHTML = WachstumgFR.toFixed(2).toString();
-    wachstumgesamttext.innerHTML = "Wachstumsrate gesamt zwischen 2020 und 2022 in Mio";
-    document.getElementById("chart").className = "stylefr";
-}
 document.querySelector("#it").addEventListener("click", function () {
-    it22();
+    changeTo(populationIT22, populationIT20, ProzentES, WachstumgIT, "styleit");
 });
-function it22() {
-    gesamt.innerHTML = populationIT22.toString();
-    gesamttext.innerHTML = "Gesamtzahl EinwohnerInnen in Italien in 2022 in Mio";
-    relativ.innerHTML = populationIT20.toString();
-    relativtext.innerHTML = "Gesamtzahl EinwohnerInnen in Italien im Jahr 2020 in Mio";
-    wachstum.innerHTML = ProzentIT.toFixed(2).toString();
-    wachstumtext.innerHTML = "Wachstumsrate seit 2022 in Prozent";
-    wachstumgesamt.innerHTML = WachstumgIT.toFixed(2).toString();
-    wachstumgesamttext.innerHTML = "Wachstumsrate gesamt zwischen 2020 und 2022 in Mio";
-    document.getElementById("chart").className = "styleit";
-}
 document.querySelector("#es").addEventListener("click", function () {
-    es22();
+    changeTo(populationES22, populationES20, ProzentES, WachstumgES, "stylees");
 });
-function es22() {
-    gesamt.innerHTML = populationES22.toString();
-    gesamttext.innerHTML = "Gesamtzahl EinwohnerInnen in Spanien in 2022 in Mio";
-    relativ.innerHTML = populationES20.toString();
-    relativtext.innerHTML = "Gesamtzahl EinwohnerInnen in Spanien im Jahr 2020 in Mio";
-    wachstum.innerHTML = ProzentES.toFixed(2).toString();
-    wachstumtext.innerHTML = "Wachstumsrate seit 2022 in Prozent";
-    wachstumgesamt.innerHTML = WachstumgES.toFixed(2).toString();
-    wachstumgesamttext.innerHTML = "Wachstumsrate gesamt zwischen 2020 und 2022 in Mio";
-    document.getElementById("chart").className = "stylees";
-}
 document.querySelector("#eu").addEventListener("click", function () {
-    eu22();
+    changeTo(populationEU22, populationEU20, ProzentEU, WachstumgEU, "styleeu");
 });
-function eu22() {
-    gesamt.innerHTML = populationEU22.toString();
-    gesamttext.innerHTML = "Gesamtzahl EinwohnerInnen der EU in 2022 in Mio";
-    relativ.innerHTML = populationEU20.toString();
-    relativtext.innerHTML = "Gesamtzahl EinwohnerInnen der EU im Jahr 2020 in Mio";
-    wachstum.innerHTML = ProzentEU.toFixed(2).toString();
+function changeTo(population22, population20, Prozent, Wachstumg, style) {
+    gesamt.innerHTML = population22.toString();
+    gesamttext.innerHTML = "Gesamtzahl EinwohnerInnen in 2022 in Mio";
+    relativ.innerHTML = population20.toString();
+    relativtext.innerHTML = "Gesamtzahl EinwohnerInnen im Jahr 2020 in Mio";
+    wachstum.innerHTML = Prozent.toFixed(2).toString();
     wachstumtext.innerHTML = "Wachstumsrate seit 2022 in Prozent";
-    wachstumgesamt.innerHTML = WachstumgEU.toFixed(2).toString();
+    wachstumgesamt.innerHTML = Wachstumg.toFixed(2).toString();
     wachstumgesamttext.innerHTML = "Wachstumsrate gesamt zwischen 2020 und 2022 in Mio";
-    document.getElementById("chart").className = "styleeu";
+    document.getElementById("chart").className = style;
 }
 //# sourceMappingURL=aufgabe7.js.map
